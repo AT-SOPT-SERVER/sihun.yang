4a1be878100054a134911765c47dd349b2b224e8
Sihun23 <gcsw16@s-MackBook-Pro-16.local>
2025-04-03 03:20:05 +0900

    :tada:프로젝트 초기 세팅

---
+++ src/test/java/org/sopt/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
