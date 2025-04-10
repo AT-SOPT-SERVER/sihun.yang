7de127e8b66da2b459073263d438491cde0ea615
Sihun23 <gcsw16@s-MackBook-Pro-16.local>
2025-04-11 07:17:34 +0900

    :sparkles:feat:게시글 작성 3분 제한 구현

--- src/main/java/org/sopt/assignment/service/PostService.java
+++ src/main/java/org/sopt/assignment/service/PostService.java
@@ -3,16 +3,29 @@ package org.sopt.assignment.service;
 import org.sopt.assignment.domain.Post;
 import org.sopt.assignment.repository.PostRepository;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class PostService {
     private final PostRepository postRepository = new PostRepository();
+    private LocalDateTime lastCreatedAt = null;
 
     public void createPost(Post post) {
+        // 도배 방지
+        LocalDateTime now = LocalDateTime.now();
+        if (lastCreatedAt != null) {
+            long seconds = Duration.between(lastCreatedAt, now).getSeconds();
+            if (seconds < 180) {
+                throw new IllegalArgumentException("3분동안 게시글 작성이 제한됩니다.");
+            }
+        }
+        //중복 제목 검증
         if(postRepository.existsByTitle(post.getTitle())) {
             throw new IllegalArgumentException("이미 존재하는 제목입니다.");
         }
         postRepository.save(post);
+        lastCreatedAt = now;
     }
 
     public List<Post> getAllPosts() {
