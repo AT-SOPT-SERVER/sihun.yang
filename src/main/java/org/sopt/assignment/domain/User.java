eb83bf654850c8dec5aa8fd451f389bca0a0958c
Sihun23 <gcsw16@s-MackBook-Pro-16.local>
2025-05-03 00:47:49 +0900

    :wrench:fix:닉네임 글자수 제한 변경 20자->10자

--- src/main/java/org/sopt/assignment/domain/User.java
+++ src/main/java/org/sopt/assignment/domain/User.java
@@ -11,7 +11,7 @@ public class User {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable=false, length=20)
+    @Column(nullable=false, length=10)
     private String nickname;
 
     @OneToMany(mappedBy = "user")
--- src/main/java/org/sopt/assignment/dto/request/UserCreateRequest.java
+++ src/main/java/org/sopt/assignment/dto/request/UserCreateRequest.java
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.Size;
 
 public record UserCreateRequest(
         @NotBlank(message = "닉네임은 비어있을 수 없습니다.")
-        @Size(max = 20, message = "닉네임은 20자 이하여야 합니다.")
+        @Size(max = 10, message = "닉네임은 10자 이하여야 합니다.")
         String nickname
 ) {}
